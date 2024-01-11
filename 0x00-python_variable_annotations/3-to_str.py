c3004cf7dc1a1bf98460e773864656739f738bf3
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-11 17:57:01 +0000

    Task 3-to_str.py

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+""" Write a type-annotated function to_str that takes a float n
+    as argument and returns the string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """ To String Function """
+    return (str(n))
