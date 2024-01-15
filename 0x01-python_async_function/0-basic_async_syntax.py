e690cf5082e300280bf8612a9fa03d2f5a9992db
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-15 09:15:58 +0000

    Fix Task 0-basic_async_syntax.py

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -8,7 +8,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """ Wait Random Function """
     rand = random.uniform(0, max_delay)
     await asyncio.sleep(rand)
--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+""" Import wait_random from the previous python file that you’ve
+    written and write an async routine called wait_n that takes
+    in 2 int arguments (in this order): n and max_delay. You will
+    spawn wait_random n times with the specified max_delay.
+
+    wait_n should return the list of all the delays (float values).
+    The list of the delays should be in ascending order without
+    using sort() because of concurrency.
+"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list:
+    """ Wait N Function """
+    delays = []
+    for delay in range(n):
+        delays.append(await wait_random(max_delay))
+
+    return (delays)
