e5249ab747f9c2756890b755001aa5ed38a9809f
MahmoudEasa <MahmoudEasa@gmail.com>
2024-02-02 11:42:55 +0000

    Task 2. Mock HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,8 +2,10 @@
 """ Create a TestAccessNestedMap class that inherits from unittest.TestCase
 """
 import unittest
+from unittest.mock import patch, Mock
 from parameterized import parameterized
 access_nested_map = __import__('utils').access_nested_map
+get_json = __import__('utils').get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -26,6 +28,25 @@ class TestAccessNestedMap(unittest.TestCase):
         """ Test that a KeyError is raised """
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """ Test that utils.get_json returns the expected result """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch("requests.get")
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """ Test that utils.get_json returns the expected result """
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
+        res = get_json(test_url)
+        mock_get.assert_called_with(test_url)
+        self.assertEqual(res, test_payload)
 
 
 if __name__ == "__main__":
