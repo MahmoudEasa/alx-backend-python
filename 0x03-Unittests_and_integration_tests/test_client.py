326b43e52f08c4d1ffd43591c8f16e83bcb547e0
MahmoudEasa <MahmoudEasa@gmail.com>
2024-02-03 08:48:48 +0000

    Initial Code test_org

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+""" Test Client """
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+GithubOrgClient = __import__('client').GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """ Test GitHub Org Client Class """
+    def __init__(self):
+        self.gitHub_client_obj = GithubOrgClient()
+
+    @parameterized.expend([
+        ("google"),
+        ("abc"),
+    ])
+    @patch("get_json")
+    def test_org(self, param_arg, mock_get):
+        """ Test that GithubOrgClient.org returns the correct value """
+        pass
